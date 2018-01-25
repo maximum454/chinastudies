318ca2250af9aeb3cd15c83f9b82ae85414d467b
Maxim <maximum454@gmail.com>
2018-01-25 23:21:44 +0600

    about

--- src/js/chinastudies.js
+++ src/js/chinastudies.js
@@ -1,9 +1,21 @@
 //= partials/jquery.min.js
+//= partials/slick.min.js
 
 $(function () {
     /*link*/
     $("a[href='#']").click(function () {
         return false;
     });
+    if($main_slider.size() > 0){
+        $main_slider.slick({
+            infinite: true,
+            slidesToShow: 1,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            centerMode: true,
+            centerPadding:'0',
+            variableWidth: true
+        });
+    }
 });
 
