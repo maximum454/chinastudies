91039270463376e3c42efd24e27cfb8ab8beaa53
Maxim <maximum454@gmail.com>
2018-01-27 16:26:22 +0600

    partners-list

--- src/js/chinastudies.js
+++ src/js/chinastudies.js
@@ -6,16 +6,5 @@ $(function () {
     $("a[href='#']").click(function () {
         return false;
     });
-    if($main_slider.size() > 0){
-        $main_slider.slick({
-            infinite: true,
-            slidesToShow: 1,
-            autoplay: true,
-            autoplaySpeed: 4000,
-            centerMode: true,
-            centerPadding:'0',
-            variableWidth: true
-        });
-    }
 });
 
